5be2857c056c0e271fdd288b31e3eff589cf55c0
Tudor Rusu <rusutudor@protonmail.com>
2022-04-12 18:27:23 +0300

    Added temporary interface and basic functionality

---
+++ script.js
@@ -0,0 +1,68 @@
+const CONTAINER = document.querySelector('#container');
+const CONTAINER_WIDTH = CONTAINER.clientWidth;
+const CONTAINER_HEIGHT = CONTAINER.clientHeight;
+const INCREASE_SIZE_BUTTON = document.querySelector('#increase-size');
+const DECREASE_SIZE_BUTTON = document.querySelector('#decrease-size');
+const SIZE_DISPLAY = document.querySelector('#current-size-display');
+const CLEAR_BUTTON = document.querySelector('#clear-button');
+
+let gridSize = 32;
+let currentColor = `rgb(255, 0, 0)`;
+
+CLEAR_BUTTON.addEventListener('click', () => {
+    clearEverything();
+});
+
+INCREASE_SIZE_BUTTON.addEventListener('click', () => {
+    let tempSize = gridSize * 2;
+    if (tempSize <= 64) {
+        gridSize = tempSize;
+        clearEverything();
+    }
+
+    else if (tempSize > 64) {
+        console.log('cannot increase size any more');
+    }
+});
+
+DECREASE_SIZE_BUTTON.addEventListener('click', () => {
+    let tempSize = gridSize / 2;
+    if (tempSize >= 16) {
+        gridSize = tempSize;
+        clearEverything();
+    }
+
+    else if (tempSize < 16) {
+        console.log('cannot decrease size any more');
+    }
+});
+
+function drawGrid() {
+    for (let i = 0; i < gridSize ** 2; i++) {
+
+        let square = document.createElement('div');
+        square.className = 'square';
+        square.style.width = CONTAINER_WIDTH / gridSize + 'px';
+        square.style.height = CONTAINER_HEIGHT / gridSize + 'px';
+        CONTAINER.appendChild(square);
+
+        square.addEventListener('mouseover', (e) => {
+            if (e.buttons == 1) {
+                square.style.backgroundColor = currentColor;
+            }
+        });
+    }
+
+    SIZE_DISPLAY.textContent = gridSize;
+}
+
+function clearEverything() {
+    for (let i of document.querySelectorAll('div')) {
+        if (i.className == 'square') {
+            i.remove();
+        }
+    }
+    drawGrid();
+}
+
+drawGrid();
